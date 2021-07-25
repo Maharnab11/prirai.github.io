e3cb88de562706b8a108ee1cf0bea645431d0e63
prs <priyanshu1@outlook.in>
2021-07-26 03:17:47 +0530

    Undercover

---
+++ Oreilly/test.js
@@ -0,0 +1,11 @@
+
+$.get('https://stackoverflow.com/feeds/question/10943544', function (data) {
+    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
